cf58a01aebdb0fef76da5b124cce6c41a51f8ebd
nv.hai <105141081+hai0111@users.noreply.github.com>
2023-11-30 09:36:53 +0700

    add test

--- components/mastheadMasthead.cy.tsx
+++ components/mastheadMasthead.cy.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Masthead from './masthead'
+import '@/styles/global.css'
 
 describe('<Masthead />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Masthead />)
-  })
+	it('renders', () => {
+		// see: https://on.cypress.io/mounting-react
+		cy.mount(<Masthead />)
+		cy.get('h1').contains('Margelo')
+		cy.get('h2').contains('App Development, done right')
+	})
 })
